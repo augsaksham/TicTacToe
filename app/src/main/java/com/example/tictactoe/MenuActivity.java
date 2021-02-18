6f58a88b2cbd338f8fc4910323e80b02adecd3c5
augsaksham <sakshamdwivedi10@gmail.com>
2021-02-18 22:23:44 +0530

    Added a new background to the menu layout .Added on click methods to all the four buttons .Completed the menu layout designing .I have imported  a font family (Alconica).

--- app/src/main/java/com/example/tictactoe/MenuActivity.java
+++ app/src/main/java/com/example/tictactoe/MenuActivity.java
@@ -2,13 +2,35 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void startGame_SinglePlayer(View view) {
+        Intent i=new Intent(getApplicationContext(),GameActivity.class);
+        startActivity(i);
+    }
+
+    public void StartGameOnline(View view) {
+        Intent i=new Intent(getApplicationContext(),OnlineLoginActivity.class);
+        startActivity(i);
+    }
+
+    public void Exit(View view) {
+        int pid=android.os.Process.myPid();
+        android.os.Process.killProcess(pid);
+    }
+
+    public void OpenChat(View view) {
     }
 }
