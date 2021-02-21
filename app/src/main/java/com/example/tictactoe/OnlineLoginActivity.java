65c548c7e48649de9efceef5a85bb1b3029de123
augsaksham <sakshamdwivedi10@gmail.com>
2021-02-21 09:02:56 +0530

    Linked firebase to my app

--- app/src/main/java/com/example/tictactoe/OnlineLoginActivity.java
+++ app/src/main/java/com/example/tictactoe/OnlineLoginActivity.java
@@ -1,13 +1,17 @@
 package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 import android.os.Bundle;
 
 public class OnlineLoginActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online_login);
     }
