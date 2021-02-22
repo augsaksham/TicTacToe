57a5555f267da991512eff1440a1919c18dfcf12
augsaksham <sakshamdwivedi10@gmail.com>
2021-02-22 08:26:03 +0530

    Added the login layout and designed also made the functions to make a user and check its login state

--- app/src/main/java/com/example/tictactoe/OnlineLoginActivity.java
+++ app/src/main/java/com/example/tictactoe/OnlineLoginActivity.java
@@ -1,18 +1,67 @@
 package com.example.tictactoe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class OnlineLoginActivity extends AppCompatActivity {
     private FirebaseAnalytics mFirebaseAnalytics;
-
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online_login);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user=firebaseAuth.getCurrentUser();
+                if(user!=null){
+                    Log.d("Auth","onAuthStateChanged:signed_in:"+user.getUid());
+                }
+                else{
+                    Log.d("Auth Failed","onAuthStateChanged:signed_out or login");
+
+                }
+            }
+        };
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        if(mAuthListener!=null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    public void RegisterUser(String email,String Password){
+        mAuth.createUserWithEmailAndPassword(email,Password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("Auth","createUserWithEmail:onComplete:"+task);
+                        if(!task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"Auth failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
