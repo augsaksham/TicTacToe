4a6aca4abe9f7b37d42eb304812e07e8d66fc728
augsaksham <sakshamdwivedi10@gmail.com>
2021-03-05 09:00:12 +0530

    Completed the sending request and accepting request code .I have deleted all accounts so that newly registered users get under the users node in my database.

--- app/src/main/java/com/example/tictactoe/OnlineLoginActivity.java
+++ app/src/main/java/com/example/tictactoe/OnlineLoginActivity.java
@@ -1,132 +1,255 @@
 package com.example.tictactoe;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class OnlineLoginActivity extends AppCompatActivity  {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+
+public class OnlineLoginActivity extends AppCompatActivity {
+    ListView lv_loginUsers;
+    ArrayList<String> list_loginUsers = new ArrayList<String>();
+    ArrayAdapter adpt;
+    ListView lv_requstedUsers;
+    ArrayList<String> list_requestedUsers = new ArrayList<String>();
+    ArrayAdapter reqUsersAdpt;
+    TextView tvUserID, tvSendRequest, tvAcceptRequest;
+    String LoginUserID, UserName, LoginUID;
     private FirebaseAnalytics mFirebaseAnalytics;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    EditText etEmail;
-//    EditText etPassword;
-    Button btnLogin;
-    ListView lv_loginUsers;
-    ArrayList<String> list_loginUsers=new ArrayList<String>();
-    ArrayAdapter adpt;
-    ListView lv_requestedUsers;
-    ArrayList<String> list_requestedUsers=new ArrayList<String>();
-    ArrayAdapter reqUserAdpt;
-    TextView tvUserID,tvSendRequest,tvAcceptRequest;
-    String LoginUserID,UserName,LoginUID;
-    FirebaseDatabase database=FirebaseDatabase.getInstance();
-    DatabaseReference myRef=database.getReference();
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online_login);
-        tvAcceptRequest=(TextView)findViewById(R.id.tvAcceptRequest);
-        tvSendRequest=(TextView)findViewById(R.id.tvSendRequest);
-        tvAcceptRequest.setText("Please Wait ....");
-        tvSendRequest.setText("Please Wait ....");
+        tvSendRequest = (TextView) findViewById(R.id.tvSendRequest);
+        tvAcceptRequest = (TextView) findViewById(R.id.tvAcceptRequest);
+        tvSendRequest.setText("Please wait...");
+        tvAcceptRequest.setText("Please wait...");
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         mAuth = FirebaseAuth.getInstance();
-        lv_loginUsers=(ListView) findViewById(R.id.lv_loginUsers);
-        adpt=new ArrayAdapter(this,android.R.layout.simple_list_item_1,list_loginUsers);
+        lv_loginUsers = (ListView) findViewById(R.id.lv_loginUsers);
+        adpt = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list_loginUsers);
         lv_loginUsers.setAdapter(adpt);
-        lv_requestedUsers=(ListView) findViewById(R.id.lv_requestedUsers);
-        adpt=new ArrayAdapter(this,android.R.layout.simple_list_item_1,list_requestedUsers);
-        lv_loginUsers.setAdapter(reqUserAdpt);
-        mAuthListener=new FirebaseAuth.AuthStateListener() {
+        lv_requstedUsers = (ListView) findViewById(R.id.lv_requestedUsers);
+        reqUsersAdpt = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list_requestedUsers);
+        lv_requstedUsers.setAdapter(reqUsersAdpt);
+        tvUserID = (TextView) findViewById(R.id.tvLoginUser);
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user=firebaseAuth.getCurrentUser();
-                if(user!=null){
-                    Log.d("Auth","onAuthStateChanged:signed_in:"+user.getUid());
-                }
-                else{
-                    Intent i=new Intent(getApplicationContext(),GlobalLoginActivity.class);
-                    startActivity(i);
-                    Log.d("Auth Failed","onAuthStateChanged:signed_out or login");
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    LoginUID = user.getUid();
+                    Log.d("Auth", "onAuthStateChanged:signed_in:" + LoginUID);
+                    LoginUserID = user.getEmail();
+                    tvUserID.setText(LoginUserID);
+                    UserName = convertEmailToString(LoginUserID);
+                    UserName = UserName.replace(".", "");
+                    myRef.child("users").child(UserName).child("request").setValue(LoginUID);
+                    reqUsersAdpt.clear();
+                    AcceptIncommingRequests();
+                } else {
+                    // User is signed out
+                    Log.d("Auth", "onAuthStateChanged:signed_out");
 
                 }
             }
         };
+        myRef.getRoot().child("users").addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                updateLoginUsers(dataSnapshot);
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                tvAcceptRequest.setText("Error");
+
+            }
+        });
+        lv_loginUsers.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final String requestToUser = ((TextView)view).getText().toString();
+                confirmRequest(requestToUser, "To");
+            }
+        });
+        lv_requstedUsers.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final String requestFromUser = ((TextView)view).getText().toString();
+                confirmRequest(requestFromUser, "From");
+            }
+        });
     }
+
+
+    void confirmRequest(final String OtherPlayer, final String reqType){
+        AlertDialog.Builder b = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.connect_player_dialog, null);
+        b.setView(dialogView);
+
+        b.setTitle("Start Game?");
+        b.setMessage("Connect with " + OtherPlayer);
+        b.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                myRef.child("users")
+                        .child(OtherPlayer).child("request").push().setValue(LoginUserID);
+                if(reqType.equalsIgnoreCase("From")) {
+                    StartGame(OtherPlayer + ":" + UserName, OtherPlayer, "From");
+                }else{
+                    StartGame(UserName + ":" + OtherPlayer, OtherPlayer, "To");
+                }
+            }
+        });
+        b.setNegativeButton("Back", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        b.show();
+
+
+
+    }
+
+
+    void StartGame(String PlayerGameID, String OtherPlayer, String requestType){
+        myRef.child("playing").child(PlayerGameID).removeValue();
+        Intent intent = new Intent(getApplicationContext(), OnlineGameActivity.class);
+        intent.putExtra("player_session", PlayerGameID);
+        intent.putExtra("user_name", UserName);
+        intent.putExtra("other_player", OtherPlayer);
+        intent.putExtra("login_uid", LoginUID);
+        intent.putExtra("request_type", requestType);
+        startActivity(intent);
+    }
+
+    private String convertEmailToString(String Email){
+        String value = Email.substring(0, Email.indexOf('@'));
+        value = value.replace(".", "");
+        return value;
+    }
+
+
+    void AcceptIncommingRequests(){
+        myRef.child("users").child(UserName).child("request")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        try{
+                            HashMap<String, Object> map = (HashMap<String, Object>) dataSnapshot.getValue();
+                            if(map != null){
+                                String value = "";
+                                for(String key:map.keySet()){
+                                    value = (String) map.get(key);
+                                    reqUsersAdpt.add(convertEmailToString(value));
+                                    reqUsersAdpt.notifyDataSetChanged();
+                                    myRef.child("users").child(UserName).child("request").setValue(LoginUID);
+                                }
+                            }
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+
+                    }
+                });
+    }
+
+    public void updateLoginUsers(DataSnapshot dataSnapshot){
+        String key = "";
+        Set<String> set = new HashSet<String>();
+        Iterator i = dataSnapshot.getChildren().iterator();
+        int count=0;
+        while(i.hasNext()){
+            key = ((DataSnapshot) i.next()).getKey();
+            if(!key.equalsIgnoreCase(UserName)) {
+                set.add(key);
+                tvAcceptRequest.setText("count = "+(count++));
+            }
+        }
+
+        adpt.clear();
+        adpt.addAll(set);
+        adpt.notifyDataSetChanged();
+        tvSendRequest.setText("Send request to");
+        tvAcceptRequest.setText("Accept request from");
+    }
+
+
     @Override
     public void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
     }
+
+
     @Override
-    public void onStop(){
+    public void onStop() {
         super.onStop();
-        if(mAuthListener!=null){
+        if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
     }
-//    public void RegisterUser(String email,String Password){
-//        mAuth.createUserWithEmailAndPassword(email,Password)
-//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        Log.d("Auth","createUserWithEmail:onComplete:"+task);
-//                        if(!task.isSuccessful()){
-//                            Toast.makeText(getApplicationContext(),"Auth failed",
-//                                    Toast.LENGTH_SHORT).show();
-//                        }
-//                    }
-//                });
-//    }
-//    void Register(){
-//        AlertDialog.Builder b=new AlertDialog.Builder(this);
-//        LayoutInflater inflater=this.getLayoutInflater();
-//        final View diaglogView=inflater.inflate(R.layout.login_layout,null);
-//        b.setView(diaglogView);
-//        final EditText etEmail=(EditText) diaglogView.findViewById(R.id.etEmail);
-//        final EditText etPassword=(EditText) diaglogView.findViewById(R.id.etPassword);
-//        b.setTitle("Please Register");
-//        b.setMessage("Enter Your Email And Password For Registration");
-//        b.setPositiveButton("Register", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                RegisterUser(etEmail.getText().toString(),etPassword.getText().toString());
-//            }
-//        });
-//        b.setNegativeButton("Back", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                Intent i=new Intent(getApplicationContext(),MenuActivity.class);
-//                startActivity(i);
-//                finish();
-//            }
-//        });
-//      b.show();
-//    }
+
+
+    public void RegisterUser(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("Auth Complete", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Auth failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
 
 }
